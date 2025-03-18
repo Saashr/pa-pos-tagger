1aa553f34511fe218372e74fb06767af4e97e849
Sadia Ashraf <sadiaashraf@Sadias-MacBook-Pro-9.local>
2025-03-18 19:54:08 -0400

    initial commit

--- scripts/test/0.prep.ner.sh
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/bplank/DaNplus.git
-cp -r DaNplus/data data/danplus
-rm -rf DaNplus
-
--- scripts/test/0.prep.nlu.sh
+++
@@ -1,3 +0,0 @@
-git clone https://github.com/mainlp/xsid.git
-cp -r xsid/data/xSID-0.4 data
-
